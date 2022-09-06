bcdeffa67e6d0cde989c3e7cafe8fd2a046fbdcc
matiasott <jorgematiasott@gmail.com>
2022-09-06 20:37:14 -0300

    Creacion del Header

--- proyectoFinalArgProg/src/app/app.module.ts
+++ proyectoFinalArgProg/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAppComponent } from './componentes/logo-app/logo-app.component';
+import { RedesSocialesComponent } from './componentes/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAppComponent,
+    RedesSocialesComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
